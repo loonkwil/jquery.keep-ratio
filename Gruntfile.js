dcdda1ffdf17cb917e34236845ea7288b113b122
Peter Simon <simonpepe@gmail.com>
2014-01-10 11:42:37 +0100

    Add grunt task for running jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,17 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
   // Project configuration.
   grunt.initConfig({
     qunit: {
       files: ['test/index.html']
-    }
+    },
+    jshint: {
+      all: ['*.js', 'test/**/*.js']
+    },
   });
 
   // Task to run tests
-  grunt.registerTask('test', ['qunit']);
+  grunt.registerTask('test', ['qunit', 'jshint']);
 };
