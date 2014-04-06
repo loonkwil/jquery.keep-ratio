c4fc5458007a532ee967b17e2f08a83d89720c3e
Peter Simon <simonpepe@gmail.com>
2014-04-06 13:31:31 +0200

    Fix JS coding style
     * use single quotes for strings
     * use 4 space for indenting

--- src/jquery.keep-ratio.js
+++ src/jquery.keep-ratio.js
@@ -5,73 +5,73 @@
  * Date: Dec 10 2013
  */
 (function(window, undefined) {
-  "use strict";
+    'use strict';
 
-  var $ = window.jQuery;
-  var raf = window.requestAnimationFrame;
+    var $ = window.jQuery;
+    var raf = window.requestAnimationFrame;
 
-  /**
-   * @type {{ratio: number, calculate: string}}
-   */
-  var defaultOptions = { ratio: 4/3, calculate: 'height' };
+    /**
+     * @type {{ratio: number, calculate: string}}
+     */
+    var defaultOptions = { ratio: 4/3, calculate: 'height' };
 
-  /**
-   * @param {jQuery} $el
-   * @param {{ratio: number, calculate: string}} options
-   * @param {boolean} forceRendering
-   * @return {jQuery}
-   */
-  var resize = function($el, options, forceRendering) {
-    var resizeFunction;
-    if( options.calculate === 'height' ) {
-      var width = $el.width();
-      resizeFunction = function() {
-        $el.height(Math.round( width / options.ratio ));
-      };
-    }
-    else {
-      var height = $el.height();
-      resizeFunction = function() {
-        $el.width(Math.round( height * options.ratio ));
-      };
-    }
+    /**
+     * @param {jQuery} $el
+     * @param {{ratio: number, calculate: string}} options
+     * @param {boolean} forceRendering
+     * @return {jQuery}
+     */
+    var resize = function($el, options, forceRendering) {
+        var resizeFunction;
+        if( options.calculate === 'height' ) {
+            var width = $el.width();
+            resizeFunction = function() {
+                $el.height(Math.round( width / options.ratio ));
+            };
+        }
+        else {
+            var height = $el.height();
+            resizeFunction = function() {
+                $el.width(Math.round( height * options.ratio ));
+            };
+        }
 
-    if( forceRendering ) {
-      resizeFunction();
-    }
-    else {
-      raf(resizeFunction);
-    }
+        if( forceRendering ) {
+            resizeFunction();
+        }
+        else {
+            raf(resizeFunction);
+        }
 
-    return $el;
-  };
+        return $el;
+    };
 
-  /**
-   * @param {jQuery} $el
-   * @param {{ratio: number, calculate: string}} options
-   * @param {boolean} forceRendering
-   * @return {jQuery}
-   */
-  var resizeAll = function($els, options, forceRendering) {
-    return $els.each(function() {
-      resize($(this), options, forceRendering);
-    });
-  };
+    /**
+     * @param {jQuery} $el
+     * @param {{ratio: number, calculate: string}} options
+     * @param {boolean} forceRendering
+     * @return {jQuery}
+     */
+    var resizeAll = function($els, options, forceRendering) {
+        return $els.each(function() {
+            resize($(this), options, forceRendering);
+        });
+    };
 
 
-  /**
-   * @param {{ratio: number, calculate: string}} options
-   * @return {jQuery}
-   */
-  $.fn.keepRatio = function(options) {
-    options = $.extend({}, defaultOptions, options);
+    /**
+     * @param {{ratio: number, calculate: string}} options
+     * @return {jQuery}
+     */
+    $.fn.keepRatio = function(options) {
+        options = $.extend({}, defaultOptions, options);
 
-    var $boxes = this;
+        var $boxes = this;
 
-    $(window).on('resize', function() {
-      resizeAll($boxes, options);
-    });
+        $(window).on('resize', function() {
+            resizeAll($boxes, options);
+        });
 
-    return resizeAll($boxes, options, true);
-  };
+        return resizeAll($boxes, options, true);
+    };
 }(window));
